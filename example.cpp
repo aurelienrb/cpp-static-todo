7d9e5f706a5eaa66b091b5b987c12bd4343f3218
aurelienrb <arb@cyberkarma.net>
2018-11-29 01:01:15 +0100

    Add static_todo.hpp with TODO_BEFORE() / FIXME_BEFORE() macros
    Also add test file with CI integration
    
    a

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include "static_todo.hpp"
+
+// global namespace
+TODO_BEFORE(12, 2018, "update this example to pass the CI");
+
+int main() {
+    // inside a function
+    FIXME_BEFORE(01, 2019, "update this example to pass the CI");
+}
